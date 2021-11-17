259e32480594fccfdc8fca3e59bdd977e92f8d20
Mohammad <s184174@student.dtu.dk>
2021-11-17 15:48:43 +0100

    splashscreen

--- app/src/main/java/com/example/aluboxapp/MainActivity.java
+++ app/src/main/java/com/example/aluboxapp/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.aluboxapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
+
+        Toast.makeText(this,"Hello", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/aluboxapp/SplashActivity.java
+++ app/src/main/java/com/example/aluboxapp/SplashActivity.java
@@ -1,5 +1,35 @@
 package com.example.aluboxapp;
 
-public class SplashActivity {
-    //
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class SplashActivity extends Activity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        // deprecated after api 30
+        // this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        // WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+
+        handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
 }
